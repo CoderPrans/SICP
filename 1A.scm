6c98f2404efba7c748f22a98b93af10918a37520
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2021-06-29 15:03:26 +0530

    Initial Commit

---
+++ 1A.scm
@@ -0,0 +1,12 @@
+
+
+(define (root x)
+  (define square (lambda (x) (* x x)))
+  (define avg (lambda (x y) (/ (+ x y) 2)))
+  (define good? (lambda (g) (< (abs (- (square g) x)) 0.001)))
+  (define improv (lambda (g) (avg g (/ x g))))
+  (define try (lambda (g) (if (good? g) g (try (improv g)))))
+  (try 1))
+
+(root 2)
+(exact->inexact (root 2))
